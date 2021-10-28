0c91d3d73ac53005ec547ed87af5aa2f3d15c6b4
itsmearss <annurriyadhus17@gmail.com>
2021-10-28 20:00:58 +0700

    first commit

---
+++ src/main/kotlin/Variabel.kt
@@ -0,0 +1,44 @@
+fun main() {
+    var kardus = 10
+    kardus = 20
+    println(kardus)
+
+    //Bilangan Bulat
+    var angkabyte : Byte = 120
+    var angkashort : Short = 32_000
+    var angkaint : Int = 100_000_000
+    var angkalong : Long = 500_000_000
+
+    //Bilangan Desimal
+    var angkafloat : Float = 3.14F
+    var angkadouble : Double = 3.1111114
+
+    //Tipe Data Char
+    var datachar : Char = 'A'
+
+    //Tipe Data String
+    var nama : String = "Annur"
+    var namalengkap : String = """
+        ANNUR
+        RIYADHUS
+        SOLIKHIN
+    """.trimIndent()
+
+    var benar : Boolean = true
+    var salah : Boolean = false
+
+    println(angkabyte)
+    println(angkashort)
+    println(angkaint)
+    println(angkalong)
+    println(angkafloat)
+    println(angkadouble)
+    println(datachar)
+    println(nama)
+    println(namalengkap)
+    println(benar)
+    println(salah)
+
+//    val ember = "Airrr"
+//    println(ember)
+}
